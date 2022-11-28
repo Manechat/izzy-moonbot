a3758bc3c5b330d4b0ad4fd49a893e8db6a7dad3
CloudburstSys <leah@minteck.org>
2022-11-28 16:22:04 +0000

    Add bot logs to ModLoggingService.cs

--- Izzy-Moonbot/Service/ModLoggingService.cs
+++ Izzy-Moonbot/Service/ModLoggingService.cs
@@ -23,20 +23,87 @@ public class ModLoggingService
         _batchLogger = new BatchLogger(_config);
     }
 
+    public BotLogBuilder CreateBotLog(SocketGuild guild)
+    {
+        return new BotLogBuilder(_config, guild, _batchLogger);
+    }
+
     public ModLogBuilder CreateModLog(SocketGuild guild)
     {
         return new ModLogBuilder(_config, guild, _batchLogger);
     }
 }
 
-public class ModLog
+public class Log
 {
     public SocketTextChannel Channel;
     public string? Content;
     public Embed? Embed;
     public string? FileLogContent;
+    public FileAttachment? Attachment;
 
-    public ModLog(SocketTextChannel channel) { Channel = channel; }
+    public Log(SocketTextChannel channel) { Channel = channel; }
+}
+
+public class BotLogBuilder
+{
+    private readonly SocketGuild _guild;
+    private readonly Config _config;
+    private readonly BatchLogger _batchLogger;
+
+    private readonly Log _log;
+
+    public BotLogBuilder(Config config, SocketGuild guild, BatchLogger batchLogger)
+    {
+        _config = config;
+        _guild = guild;
+        _batchLogger = batchLogger;
+
+        _log = new Log(_guild.GetTextChannel(_config.LogChannel));
+    }
+
+    public BotLogBuilder SetContent(string content)
+    {
+        _log.Content = content;
+        return this;
+    }
+
+    public BotLogBuilder SetEmbed(Embed embed)
+    {
+        _log.Embed = embed;
+        return this;
+    }
+
+    public BotLogBuilder SetFileLogContent(string content)
+    {
+        _log.FileLogContent = content;
+        return this;
+    }
+    
+    public BotLogBuilder SetFileAttachment(FileAttachment attachment)
+    {
+        _log.Attachment = attachment;
+        return this;
+    }
+
+    public async Task Send()
+    {
+        if (_log.Content == null && _log.Embed == null) throw new InvalidOperationException("A bot log cannot have no content");
+
+        if (_config.BatchSendLogs)
+            _batchLogger.AddBotLog(_log);
+        else
+        {
+            if (_log.Attachment != null)
+            {
+                await _log.Channel.SendFileAsync(_log.Attachment.Value, _log.Content, embed: _log.Embed);
+            }
+            else
+            {
+                await _log.Channel.SendMessageAsync(_log.Content, embed: _log.Embed);
+            }
+        }
+    }
 }
 
 public class ModLogBuilder
@@ -45,7 +112,7 @@ public class ModLogBuilder
     private readonly Config _config;
     private readonly BatchLogger _batchLogger;
 
-    private readonly ModLog _log;
+    private readonly Log _log;
 
     public ModLogBuilder(Config config, SocketGuild guild, BatchLogger batchLogger)
     {
@@ -53,7 +120,7 @@ public class ModLogBuilder
         _guild = guild;
         _batchLogger = batchLogger;
 
-        _log = new ModLog(_guild.GetTextChannel(_config.ModChannel));
+        _log = new Log(_guild.GetTextChannel(_config.ModChannel));
     }
 
     public ModLogBuilder SetContent(string content)
@@ -74,6 +141,12 @@ public class ModLogBuilder
         return this;
     }
 
+    public ModLogBuilder SetFileAttachment(FileAttachment attachment)
+    {
+        _log.Attachment = attachment;
+        return this;
+    }
+
     public async Task Send()
     {
         if (_log.Content == null && _log.Embed == null) throw new InvalidOperationException("A moderation log cannot have no content");
@@ -87,19 +160,29 @@ public class ModLogBuilder
             if (!File.Exists(filepath))
                 await File.WriteAllTextAsync(filepath, $"----------= {DateTimeOffset.UtcNow:F} =----------{Environment.NewLine}");
 
-            await File.AppendAllTextAsync(filepath, modLogFileContent);
+            await File.AppendAllTextAsync(filepath, $"{modLogFileContent}{(_log.Attachment != null ? " [ATTACHMENT]" : "")}");
         }
 
         if (_config.BatchSendLogs)
             _batchLogger.AddModLog(_log);
         else
-            await _log.Channel.SendMessageAsync(_log.Content, embed: _log.Embed);
+        {
+            if (_log.Attachment != null)
+            {
+                await _log.Channel.SendFileAsync(_log.Attachment.Value, _log.Content, embed: _log.Embed);
+            }
+            else
+            {
+                await _log.Channel.SendMessageAsync(_log.Content, embed: _log.Embed);
+            }
+        }
     }
 }
 
 public class BatchLogger
 {
-    private readonly List<ModLog> _modLogs = new();
+    private readonly List<Log> _botLogs = new();
+    private readonly List<Log> _modLogs = new();
     private readonly Config _config;
 
     public BatchLogger(Config config)
@@ -109,7 +192,12 @@ public class BatchLogger
         RefreshBatchInterval();
     }
 
-    public void AddModLog(ModLog log)
+    public void AddBotLog(Log log)
+    {
+        _botLogs.Add(log);
+    }
+    
+    public void AddModLog(Log log)
     {
         _modLogs.Add(log);
     }
@@ -120,21 +208,61 @@ public class BatchLogger
         {
             await Task.Delay(Convert.ToInt32(_config.BatchLogsSendRate * 1000));
 
+            SocketTextChannel? botLogChannel = null;
+            var botLogContent = new List<string>();
+            var botLogEmbeds = new List<Embed>();
+            var botLogAttachments = new List<FileAttachment>();
+            
             SocketTextChannel? modLogChannel = null;
             var modLogContent = new List<string>();
             var modLogEmbeds = new List<Embed>();
+            var modLogAttachments = new List<FileAttachment>();
 
+            foreach (var botLog in _botLogs)
+            {
+                botLogChannel = botLog.Channel;
+                if (botLog.Embed is not null) botLogEmbeds.Add(botLog.Embed);
+                if (botLog.Content is not null) botLogContent.Add(botLog.Content);
+                if (botLog.Attachment is not null) botLogAttachments.Add(botLog.Attachment.Value);
+            }
+            
             foreach (var modLog in _modLogs)
             {
                 modLogChannel = modLog.Channel;
                 if (modLog.Embed is not null) modLogEmbeds.Add(modLog.Embed);
                 if (modLog.Content is not null) modLogContent.Add(modLog.Content);
+                if (modLog.Attachment is not null) modLogAttachments.Add(modLog.Attachment.Value);
+            }
+            
+            if (botLogChannel != null)
+            {
+                if (botLogAttachments.Any())
+                {
+                    await botLogChannel.SendFilesAsync(botLogAttachments,
+                        $"{string.Join(Environment.NewLine, botLogContent)}{Environment.NewLine}{Environment.NewLine}**Note:** Files may not be listed in the correct order.", embeds: botLogEmbeds.ToArray());
+                }
+                else
+                {
+                    await botLogChannel.SendMessageAsync(string.Join($"{Environment.NewLine}", botLogContent),
+                        embeds: botLogEmbeds.ToArray());
+                }
             }
 
             if (modLogChannel != null)
-                await modLogChannel.SendMessageAsync(string.Join($"{Environment.NewLine}", modLogContent),
-                    embeds: modLogEmbeds.ToArray());
-            
+            {
+                if (modLogAttachments.Any())
+                {
+                    await modLogChannel.SendFilesAsync(modLogAttachments,
+                        $"{string.Join(Environment.NewLine, modLogContent)}{Environment.NewLine}{Environment.NewLine}**Note:** Files may not be listed in the correct order.", embeds: modLogEmbeds.ToArray());
+                }
+                else
+                {
+                    await modLogChannel.SendMessageAsync(string.Join($"{Environment.NewLine}", modLogContent),
+                        embeds: modLogEmbeds.ToArray());
+                }
+            }
+
+            _botLogs.Clear();
             _modLogs.Clear();
 
             RefreshBatchInterval();
