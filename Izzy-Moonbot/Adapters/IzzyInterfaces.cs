c440ddf7380417d70bb426648835c248157d674b
ixrec <ericrhitchcock@gmail.com>
2022-11-28 22:15:41 +0000

    create test doubling interfaces, and adapters that implement them with the Discord.NET types

---
+++ Izzy-Moonbot/Adapters/IzzyInterfaces.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Izzy_Moonbot.Adapters;
+
+public interface IIzzyUser
+{
+    ulong Id { get; }
+}
+
+public interface IIzzyRole
+{
+    string Name { get; }
+    ulong Id { get; }
+}
+
+public interface IIzzySocketMessageChannel
+{
+    ulong Id { get; }
+    string Name { get; }
+    Task<IIzzyUser> GetUserAsync(ulong userId);
+}
+
+public interface IIzzySocketTextChannel
+{
+    ulong Id { get; }
+    string Name { get; }
+    IReadOnlyCollection<IIzzyUser> Users { get; }
+}
+
+public interface IIzzyGuild
+{
+    ulong Id { get; }
+    Task<IReadOnlyCollection<IIzzyUser>> SearchUsersAsync(string userSearchQuery);
+    IReadOnlyCollection<IIzzySocketTextChannel> TextChannels { get; }
+    IReadOnlyCollection<IIzzyRole> Roles { get; }
+}
+
+public interface IIzzyClient
+{
+    IIzzyUser CurrentUser { get; }
+    IReadOnlyCollection<IIzzyGuild> Guilds { get; }
+}
+
+public interface IIzzyContext
+{
+    bool IsPrivate { get; }
+    IIzzyGuild Guild { get; }
+    IIzzyClient Client { get; }
+    IIzzySocketMessageChannel Channel { get; }
+}
+
