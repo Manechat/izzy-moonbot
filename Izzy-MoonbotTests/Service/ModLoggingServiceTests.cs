86cbe5e5b680f89597d6e51eab0199eb0595e1ef
ixrec <ericrhitchcock@gmail.com>
2022-12-04 02:09:18 +0000

    delete batch logging test until we can invest in solving the Task.Delay problem

--- Izzy-MoonbotTests/Service/ModLoggingServiceTests.cs
+++ Izzy-MoonbotTests/Service/ModLoggingServiceTests.cs
@@ -28,53 +28,5 @@ public class ModLoggingServiceTests
         Assert.AreEqual("Attention: It's T.U.E.S. Day!", modChat.Messages.Last().Content);
     }
 
-    [TestMethod()]
-    public async Task BatchLog_Tests()
-    {
-        var (_, _, (_, _), _, (_, modChat, _), guild, client) = TestUtils.DefaultStubs();
-        var cfg = new Config();
-        var modLog = new ModLoggingService(cfg);
-
-        cfg.ModChannel = modChat.Id;
-        cfg.BatchSendLogs = true;
-        cfg.BatchLogsSendRate = 0.01; // not realistic, but it's not worth making the test slow
-
-        await modLog.CreateModLog(new TestGuild(guild, client))
-            .SetContent($"Attention: It's T.U.E.S. Day!")
-            .Send();
-
-        Assert.AreEqual(0, modChat.Messages.Count);
-
-        await Task.Delay(100);
-
-        Assert.AreEqual(1, modChat.Messages.Count);
-        Assert.AreEqual("Attention: It's T.U.E.S. Day!", modChat.Messages.Last().Content);
-
-        await modLog.CreateModLog(new TestGuild(guild, client))
-            .SetContent($"Señor Butterscotch has joined the server.")
-            .Send();
-        await modLog.CreateModLog(new TestGuild(guild, client))
-            .SetContent($"Grandma Figgy has joined the server.")
-            .Send();
-        await modLog.CreateModLog(new TestGuild(guild, client))
-            .SetContent($"Misty Brightdawn has joined the server.")
-            .Send();
-        await modLog.CreateModLog(new TestGuild(guild, client))
-            .SetContent($"Alphabittle has joined the server.")
-            .Send();
-        await modLog.CreateModLog(new TestGuild(guild, client))
-            .SetContent($"Queen Haven has joined the server.")
-            .Send();
-
-        Assert.AreEqual(1, modChat.Messages.Count);
-
-        await Task.Delay(100);
-
-        Assert.AreEqual(2, modChat.Messages.Count);
-        Assert.AreEqual($"Señor Butterscotch has joined the server.{Environment.NewLine}" +
-            $"Grandma Figgy has joined the server.{Environment.NewLine}" +
-            $"Misty Brightdawn has joined the server.{Environment.NewLine}" +
-            $"Alphabittle has joined the server.{Environment.NewLine}" +
-            $"Queen Haven has joined the server.", modChat.Messages.Last().Content);
-    }
+    // TODO: test batch logging if we can figure out how to deal with Task.Delay()
 }
