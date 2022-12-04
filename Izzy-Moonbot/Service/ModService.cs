51da3347581f01dd196304c69ca5d5188c351281
ixrec <ericrhitchcock@gmail.com>
2022-12-04 04:28:15 +0000

    delete unused ModService methods

--- Izzy-Moonbot/Service/ModService.cs
+++ Izzy-Moonbot/Service/ModService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using Discord;
 using Discord.WebSocket;
 using Izzy_Moonbot.Helpers;
 using Izzy_Moonbot.Settings;
@@ -19,21 +18,6 @@ public class ModService
     {
         _config = config;
         _users = users;
-    }
-
-    public async Task KickUser(SocketGuildUser user, DateTimeOffset time, string? reason = null)
-    {
-        await user.KickAsync(reason);
-    }
-
-    public async Task KickUsers(IEnumerable<SocketGuildUser> users, string? reason = null)
-    {
-        if (!users.Any()) throw new NullReferenceException("users must have users in them");
-
-        foreach (var user in users)
-        {
-            await user.KickAsync(reason);
-        }
     }
 
     public async Task SilenceUser(SocketGuildUser user, string? reason = null)
@@ -66,61 +50,13 @@ public class ModService
         await user.AddRoleAsync(roleId);
     }
 
-    public async Task AddRoleToUsers(IEnumerable<SocketGuildUser> users, ulong roleId, string? reason = null)
-    {
-        if (!users.Any()) throw new NullReferenceException("users must have users in them");
-
-        foreach (var socketGuildUser in users)
-        {
-            await socketGuildUser.AddRoleAsync(roleId);
-        }
-    }
-
     public async Task RemoveRole(SocketGuildUser user, ulong roleId, string? reason = null)
     {
         await user.RemoveRoleAsync(roleId);
     }
 
-    public async Task RemoveRoleFromUsers(IEnumerable<SocketGuildUser> users, ulong roleId, string? reason = null)
-    {
-        if (!users.Any()) throw new NullReferenceException("users must have users in them");
-
-        foreach (var socketGuildUser in users)
-        {
-            await socketGuildUser.RemoveRoleAsync(roleId);
-        }
-    }
-
     public async Task AddRoles(SocketGuildUser user, IEnumerable<ulong> roles, string? reason = null)
     {
         await user.AddRolesAsync(roles);
-    }
-
-    public async Task AddRolesToUsers(IEnumerable<SocketGuildUser> users, IEnumerable<ulong> roles, string? reason = null)
-    {
-        if (!users.Any()) throw new NullReferenceException("users must have users in them");
-        if (!roles.Any()) throw new NullReferenceException("roles must have role ids in them");
-
-        foreach (var socketGuildUser in users)
-        {
-            await socketGuildUser.AddRolesAsync(roles);
-        }
-    }
-
-    public async Task RemoveRoles(SocketGuildUser user, IEnumerable<ulong> roles, string? reason = null)
-    {
-        if (!roles.Any()) throw new NullReferenceException("roles must have role ids in them");
-        await user.RemoveRolesAsync(roles);
-    }
-
-    public async Task RemoveRolesFromUsers(IEnumerable<SocketGuildUser> users, IEnumerable<ulong> roles, string? reason = null)
-    {
-        if (!users.Any()) throw new NullReferenceException("users must have users in them");
-        if (!roles.Any()) throw new NullReferenceException("roles must have role ids in them");
-
-        foreach (var socketGuildUser in users)
-        {
-            await socketGuildUser.RemoveRolesAsync(roles);
-        }
     }
 }
