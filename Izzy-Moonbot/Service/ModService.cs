4f62aafc3826daf8d1da8a87ca18294ac4ee0294
ixrec <ericrhitchcock@gmail.com>
2022-12-04 04:29:14 +0000

    actually use the reason string, since some call sites are passing it

--- Izzy-Moonbot/Service/ModService.cs
+++ Izzy-Moonbot/Service/ModService.cs
@@ -24,7 +24,7 @@ public class ModService
     {
         if (_config.MemberRole == null) throw new TargetException("MemberRole config value is null (not set)");
         
-        await user.RemoveRoleAsync((ulong) _config.MemberRole);
+        await user.RemoveRoleAsync((ulong) _config.MemberRole, reason is null ? null : new Discord.RequestOptions { AuditLogReason = reason });
             
         _users[user.Id].Silenced = true;
         await FileHelper.SaveUsersAsync(_users);
@@ -38,7 +38,7 @@ public class ModService
 
         foreach (var user in users)
         {
-            await user.RemoveRoleAsync((ulong)_config.MemberRole);
+            await user.RemoveRoleAsync((ulong)_config.MemberRole, reason is null ? null : new Discord.RequestOptions { AuditLogReason = reason });
 
             _users[user.Id].Silenced = true;
             await FileHelper.SaveUsersAsync(_users);
@@ -47,16 +47,16 @@ public class ModService
 
     public async Task AddRole(SocketGuildUser user, ulong roleId, string? reason = null)
     {
-        await user.AddRoleAsync(roleId);
+        await user.AddRoleAsync(roleId, reason is null ? null : new Discord.RequestOptions { AuditLogReason = reason });
     }
 
     public async Task RemoveRole(SocketGuildUser user, ulong roleId, string? reason = null)
     {
-        await user.RemoveRoleAsync(roleId);
+        await user.RemoveRoleAsync(roleId, reason is null ? null : new Discord.RequestOptions { AuditLogReason = reason });
     }
 
     public async Task AddRoles(SocketGuildUser user, IEnumerable<ulong> roles, string? reason = null)
     {
-        await user.AddRolesAsync(roles);
+        await user.AddRolesAsync(roles, reason is null ? null : new Discord.RequestOptions { AuditLogReason = reason });
     }
 }
