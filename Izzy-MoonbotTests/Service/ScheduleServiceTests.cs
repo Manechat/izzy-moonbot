1fea96762d69494b268bc93cc7de7bd9929616a0
ixrec <ericrhitchcock@gmail.com>
2022-12-28 22:03:42 +0000

    add tests for long, non-relative repeats

--- Izzy-MoonbotTests/Service/ScheduleServiceTests.cs
+++ Izzy-MoonbotTests/Service/ScheduleServiceTests.cs
@@ -17,18 +17,8 @@ namespace Izzy_Moonbot_Tests.Services;
 [TestClass()]
 public class ScheduleServiceTests
 {
-    [TestMethod()]
-    public async Task Breathing_Tests()
+    public static ScheduleService SetupScheduleService(Config cfg, Dictionary<ulong, User> users)
     {
-        var (cfg, _, (_, sunny), _, (generalChannel, modChat, _), guild, client) = TestUtils.DefaultStubs();
-        DiscordHelper.DefaultGuildId = guild.Id;
-
-        cfg.ModChannel = modChat.Id;
-
-        // ModService assumes that every user it's asked to silence is already in the users Dictionary
-        var users = new Dictionary<ulong, User>();
-        users[sunny.Id] = new User();
-
         var generalStorage = new GeneralStorage();
 
         var scheduledJobs = new List<ScheduledJob>();
@@ -36,7 +26,22 @@ public class ScheduleServiceTests
         var mod = new ModService(cfg, users);
         var modLog = new ModLoggingService(cfg);
         var logger = new LoggingService(new TestLogger<Worker>());
-        var ss = new ScheduleService(cfg, mod, modLog, logger, generalStorage, scheduledJobs);
+
+        return new ScheduleService(cfg, mod, modLog, logger, generalStorage, scheduledJobs);
+    }
+
+    [TestMethod()]
+    public async Task Breathing_Tests()
+    {
+        var (cfg, _, (_, sunny), _, (generalChannel, modChat, _), guild, client) = TestUtils.DefaultStubs();
+        DiscordHelper.DefaultGuildId = guild.Id;
+        cfg.ModChannel = modChat.Id;
+
+        // ModService assumes that every user it's asked to silence is already in the users Dictionary
+        var users = new Dictionary<ulong, User>();
+        users[sunny.Id] = new User();
+
+        var ss = SetupScheduleService(cfg, users);
 
         Assert.AreEqual(0, generalChannel.Messages.Count);
 
@@ -77,12 +82,7 @@ public class ScheduleServiceTests
         guild.Users.ForEach(u => users[u.Id] = new User());
         users[sproutId] = new User();
 
-        var generalStorage = new GeneralStorage();
-        var scheduledJobs = new List<ScheduledJob>();
-        var mod = new ModService(cfg, users);
-        var modLog = new ModLoggingService(cfg);
-        var logger = new LoggingService(new TestLogger<Worker>());
-        var ss = new ScheduleService(cfg, mod, modLog, logger, generalStorage, scheduledJobs);
+        var ss = SetupScheduleService(cfg, users);
 
         // assert initial state
         DateTimeHelper.FakeUtcNow = TestUtils.FiMEpoch;
@@ -147,5 +147,110 @@ public class ScheduleServiceTests
         TestUtils.AssertListsAreEqual(new List<ulong> { alicorn.Id }, guild.UserRoles[sunny.Id]);
         TestUtils.AssertListsAreEqual(new List<string> { "your sparkle is rainbow!" }, client.DirectMessages[sunny.Id].Select(sm => sm.Content).ToList());
         TestUtils.AssertSetsAreEqual(new HashSet<ulong>(), guild.BannedUserIds); // sprout's back in
+    }
+
+    [TestMethod()]
+    public async Task DailyEcho_Tests()
+    {
+        var (cfg, _, (_, sunny), _, (generalChannel, modChat, _), guild, client) = TestUtils.DefaultStubs();
+        DiscordHelper.DefaultGuildId = guild.Id;
+        cfg.ModChannel = modChat.Id;
+        var ss = SetupScheduleService(cfg, new Dictionary<ulong, User>());
+
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2010, 10, 10, 0, 0, 0, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(0, generalChannel.Messages.Count);
+
+        var action = new ScheduledEchoJob(generalChannel.Id, "test echo");
+        await ss.CreateScheduledJob(new ScheduledJob(DateTimeHelper.UtcNow, DateTimeHelper.UtcNow, action, ScheduledJobRepeatType.Daily));
+
+        await ss.Unicycle(client);
+        Assert.AreEqual(1, generalChannel.Messages.Count);
+        Assert.AreEqual("test echo", generalChannel.Messages.Last().Content);
+
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2010, 10, 10, 23, 59, 59, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(1, generalChannel.Messages.Count);
+
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2010, 10, 11, 0, 0, 0, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(2, generalChannel.Messages.Count);
+        Assert.AreEqual("test echo", generalChannel.Messages.Last().Content);
+    }
+
+    [TestMethod()]
+    public async Task WeeklyEcho_Tests()
+    {
+        var (cfg, _, (_, sunny), _, (generalChannel, modChat, _), guild, client) = TestUtils.DefaultStubs();
+        DiscordHelper.DefaultGuildId = guild.Id;
+        cfg.ModChannel = modChat.Id;
+        var ss = SetupScheduleService(cfg, new Dictionary<ulong, User>());
+
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2010, 10, 10, 0, 0, 0, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(0, generalChannel.Messages.Count);
+
+        var action = new ScheduledEchoJob(generalChannel.Id, "test echo");
+        await ss.CreateScheduledJob(new ScheduledJob(DateTimeHelper.UtcNow, DateTimeHelper.UtcNow, action, ScheduledJobRepeatType.Weekly));
+
+        await ss.Unicycle(client);
+        Assert.AreEqual(1, generalChannel.Messages.Count);
+        Assert.AreEqual("test echo", generalChannel.Messages.Last().Content);
+
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2010, 10, 16, 23, 59, 59, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(1, generalChannel.Messages.Count);
+
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2010, 10, 17, 0, 0, 0, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(2, generalChannel.Messages.Count);
+        Assert.AreEqual("test echo", generalChannel.Messages.Last().Content);
+    }
+
+    [TestMethod()]
+    public async Task YearlyEcho_Tests()
+    {
+        var (cfg, _, (_, sunny), _, (generalChannel, modChat, _), guild, client) = TestUtils.DefaultStubs();
+        DiscordHelper.DefaultGuildId = guild.Id;
+        cfg.ModChannel = modChat.Id;
+        var ss = SetupScheduleService(cfg, new Dictionary<ulong, User>());
+
+        // start the yearly echo on Oct 10th 2011
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2011, 10, 10, 0, 0, 0, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(0, generalChannel.Messages.Count);
+
+        var action = new ScheduledEchoJob(generalChannel.Id, "test echo");
+        await ss.CreateScheduledJob(new ScheduledJob(DateTimeHelper.UtcNow, DateTimeHelper.UtcNow, action, ScheduledJobRepeatType.Yearly));
+
+        await ss.Unicycle(client);
+        Assert.AreEqual(1, generalChannel.Messages.Count);
+        Assert.AreEqual("test echo", generalChannel.Messages.Last().Content);
+
+
+        // nothing happens on Oct 9th 2012
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2012, 10, 9, 0, 0, 0, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        // 2012 was a leap year, so if yearly repeats were naively mis-implemented as
+        // 365 days/8760 hours/etc, the second echo would happen here prematurely
+        Assert.AreEqual(1, generalChannel.Messages.Count);
+
+        // second echo happens on Oct 10th 2012
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2012, 10, 10, 0, 0, 0, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(2, generalChannel.Messages.Count);
+        Assert.AreEqual("test echo", generalChannel.Messages.Last().Content);
+
+
+        // nothing happens on Oct 9th 2013
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2013, 10, 9, 0, 0, 0, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(2, generalChannel.Messages.Count);
+
+        // third echo happens on Oct 10th 2013
+        DateTimeHelper.FakeUtcNow = new DateTimeOffset(2013, 10, 10, 0, 0, 0, TimeSpan.Zero);
+        await ss.Unicycle(client);
+        Assert.AreEqual(3, generalChannel.Messages.Count);
+        Assert.AreEqual("test echo", generalChannel.Messages.Last().Content);
     }
 }
