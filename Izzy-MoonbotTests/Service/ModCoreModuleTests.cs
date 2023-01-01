eef97263ddf9a69df8331cbf479f1a58a335ab7e
ixrec <ericrhitchcock@gmail.com>
2023-01-01 06:52:55 +0000

    cleanup 'var am's from before the AdminModule was split

--- Izzy-MoonbotTests/Service/ModCoreModuleTests.cs
+++ Izzy-MoonbotTests/Service/ModCoreModuleTests.cs
@@ -34,7 +34,7 @@ public class ModCoreModuleTests
         var (cfg, _, (izzy, sunny), _, (generalChannel, modChat, _), guild, client) = TestUtils.DefaultStubs();
         DiscordHelper.DefaultGuildId = guild.Id;
         cfg.ModChannel = modChat.Id;
-        var (ss, am) = SetupModCoreModule(cfg);
+        var (ss, mcm) = SetupModCoreModule(cfg);
 
         var pippId = guild.Users[3].Id;
         var hitchId = guild.Users[4].Id;
@@ -46,7 +46,7 @@ public class ModCoreModuleTests
 
         await client.AddMessageAsync(guild.Id, generalChannel.Id, hitchId, "anypony can make smoothies");
         var context = await client.AddMessageAsync(guild.Id, generalChannel.Id, sunny.Id, $".ban {hitchId}");
-        await am.TestableBanCommandAsync(context, $"{hitchId}");
+        await mcm.TestableBanCommandAsync(context, $"{hitchId}");
 
         StringAssert.Contains(generalChannel.Messages.Last().Content, "I've banned Hitch (5)");
         TestUtils.AssertSetsAreEqual(new HashSet<ulong> { hitchId }, guild.BannedUserIds);
@@ -56,7 +56,7 @@ public class ModCoreModuleTests
 
         await client.AddMessageAsync(guild.Id, generalChannel.Id, pippId, "which one of you broke my phone!?");
         context = await client.AddMessageAsync(guild.Id, generalChannel.Id, sunny.Id, $".ban <@{pippId}> 5 minutes");
-        await am.TestableBanCommandAsync(context, $"<@{pippId}> 5 minutes");
+        await mcm.TestableBanCommandAsync(context, $"<@{pippId}> 5 minutes");
 
         StringAssert.Contains(generalChannel.Messages.Last().Content, "I've banned Pipp (4)");
         TestUtils.AssertSetsAreEqual(new HashSet<ulong> { hitchId, pippId }, guild.BannedUserIds);
@@ -65,7 +65,7 @@ public class ModCoreModuleTests
         // changing an existing ban from indefinite to finite
 
         context = await client.AddMessageAsync(guild.Id, generalChannel.Id, sunny.Id, $".ban {hitchId} 10 minutes");
-        await am.TestableBanCommandAsync(context, $"{hitchId} 10 minutes");
+        await mcm.TestableBanCommandAsync(context, $"{hitchId} 10 minutes");
 
         StringAssert.Contains(generalChannel.Messages.Last().Content, "This user is already banned. I have scheduled an unban");
         TestUtils.AssertSetsAreEqual(new HashSet<ulong> { hitchId, pippId }, guild.BannedUserIds);
@@ -90,7 +90,7 @@ public class ModCoreModuleTests
         // .ban myself easter egg
 
         context = await client.AddMessageAsync(guild.Id, generalChannel.Id, sunny.Id, $".ban <@{izzy.Id}> 5 minutes");
-        await am.TestableBanCommandAsync(context, $"<@{izzy.Id}> 5 minutes");
+        await mcm.TestableBanCommandAsync(context, $"<@{izzy.Id}> 5 minutes");
 
         // randomly selected emoji, but no actual banning
         TestUtils.AssertSetsAreEqual(new HashSet<ulong>(), guild.BannedUserIds);
@@ -103,7 +103,7 @@ public class ModCoreModuleTests
         var (cfg, _, (_, sunny), roles, (generalChannel, modChat, _), guild, client) = TestUtils.DefaultStubs();
         DiscordHelper.DefaultGuildId = guild.Id;
         cfg.ModChannel = modChat.Id;
-        var (ss, am) = SetupModCoreModule(cfg);
+        var (ss, mcm) = SetupModCoreModule(cfg);
 
         var alicornId = roles[0].Id;
         var pippId = guild.Users[3].Id;
@@ -116,7 +116,7 @@ public class ModCoreModuleTests
         // .assignrole with no duration
 
         var context = await client.AddMessageAsync(guild.Id, generalChannel.Id, sunny.Id, $".assignrole <@&{alicornId}> {hitchId}");
-        await am.TestableAssignRoleCommandAsync(context, $"<@&{alicornId}> {hitchId}");
+        await mcm.TestableAssignRoleCommandAsync(context, $"<@&{alicornId}> {hitchId}");
 
         Assert.AreEqual(generalChannel.Messages.Last().Content, $"I've given <@&{alicornId}> to <@{hitchId}>.");
         Assert.IsFalse(guild.UserRoles.ContainsKey(pippId));
@@ -126,7 +126,7 @@ public class ModCoreModuleTests
         // .assignrole with duration
 
         context = await client.AddMessageAsync(guild.Id, generalChannel.Id, sunny.Id, $".assignrole <@&{alicornId}> <@{pippId}> 5 minutes");
-        await am.TestableAssignRoleCommandAsync(context, $"<@&{alicornId}> <@{pippId}> 5 minutes");
+        await mcm.TestableAssignRoleCommandAsync(context, $"<@&{alicornId}> <@{pippId}> 5 minutes");
 
         Assert.AreEqual(generalChannel.Messages.Last().Content, $"I've given <@&{alicornId}> to <@{pippId}>. I've scheduled a removal <t:1286669100:R>.");
         TestUtils.AssertListsAreEqual(new List<ulong> { alicornId }, guild.UserRoles[pippId]);
@@ -136,7 +136,7 @@ public class ModCoreModuleTests
         // changing an existing role assignment from indefinite to finite
 
         context = await client.AddMessageAsync(guild.Id, generalChannel.Id, sunny.Id, $".assignrole <@&{alicornId}> {hitchId} 10 minutes");
-        await am.TestableAssignRoleCommandAsync(context, $"<@&{alicornId}> {hitchId} 10 minutes");
+        await mcm.TestableAssignRoleCommandAsync(context, $"<@&{alicornId}> {hitchId} 10 minutes");
 
         StringAssert.Contains(generalChannel.Messages.Last().Content, $"<@{hitchId}> already has that role. I've scheduled a removal <t:1286669400:R>.");
         TestUtils.AssertListsAreEqual(new List<ulong> { alicornId }, guild.UserRoles[pippId]);
@@ -166,7 +166,7 @@ public class ModCoreModuleTests
         var (cfg, _, (_, sunny), roles, (generalChannel, modChat, _), guild, client) = TestUtils.DefaultStubs();
         DiscordHelper.DefaultGuildId = guild.Id;
         cfg.ModChannel = modChat.Id;
-        var (ss, am) = SetupModCoreModule(cfg);
+        var (ss, mcm) = SetupModCoreModule(cfg);
 
         var alicornId = roles[0].Id;
         var pippId = guild.Users[3].Id;
@@ -175,7 +175,7 @@ public class ModCoreModuleTests
         Assert.IsFalse(guild.UserRoles.ContainsKey(pippId));
 
         var context = await client.AddMessageAsync(guild.Id, generalChannel.Id, sunny.Id, $".assignrole <@&{alicornId}>   <@{pippId}>   5 minutes");
-        await am.TestableAssignRoleCommandAsync(context, $"<@&{alicornId}>   <@{pippId}>   5 minutes");
+        await mcm.TestableAssignRoleCommandAsync(context, $"<@&{alicornId}>   <@{pippId}>   5 minutes");
 
         Assert.AreEqual(generalChannel.Messages.Last().Content, $"I've given <@&{alicornId}> to <@{pippId}>. I've scheduled a removal <t:1286669100:R>.");
         TestUtils.AssertListsAreEqual(new List<ulong> { alicornId }, guild.UserRoles[pippId]);
