3d22d8227b02744980e33f3ec4c95acb58f2e0d5
CloudburstSys <leah@minteck.org>
2023-01-06 09:12:02 +0000

    Add default values to DatabaseSettings.cs

--- Izzy-Moonbot/Settings/DatabaseSettings.cs
+++ Izzy-Moonbot/Settings/DatabaseSettings.cs
@@ -4,10 +4,10 @@ namespace Izzy_Moonbot.Settings;
 
 public class DatabaseSettings
 {
-    public string Protocol { get; set; }
-    public string User { get; set; }
-    public string Password { get; set; }
-    public string Host { get; set; }
-    public string Database { get; set; }
-    public Dictionary<string, string> Options { get; set; }
+    public string Protocol { get; set; } = "";
+    public string User { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string Host { get; set; } = "";
+    public string Database { get; set; } = "";
+    public Dictionary<string, string> Options { get; set; } = new();
 }
