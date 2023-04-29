f8c6ac96e2a249ea5f34310e03e4fad0178e0084
ixrec <ericrhitchcock@gmail.com>
2023-04-29 20:54:34 +0100

    #318: give .ass a proper error message if SilenceUsers throws

--- Izzy-Moonbot/Modules/RaidModule.cs
+++ Izzy-Moonbot/Modules/RaidModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord.Commands;
@@ -6,6 +7,7 @@ using Izzy_Moonbot.Attributes;
 using Izzy_Moonbot.Helpers;
 using Izzy_Moonbot.Service;
 using Izzy_Moonbot.Settings;
+using Serilog;
 
 namespace Izzy_Moonbot.Modules;
 
@@ -39,11 +41,19 @@ public class RaidModule : ModuleBase<SocketCommandContext>
     [DevCommand(Group = "Permissions")]
     public async Task AssAsync()
     {
+        var recentJoins = _raidService.GetRecentJoins(Context.Guild);
+        try
+        {
+            await _modService.SilenceUsers(recentJoins, "Suspected raider");
+        }
+        catch (Exception ex)
+        {
+            await ReplyAsync($"Failed to silence recent joins:\n{ex.Message}");
+            return;
+        }
+
         _config.AutoSilenceNewJoins = true;
         await FileHelper.SaveConfigAsync(_config);
-
-        var recentJoins = _raidService.GetRecentJoins(Context.Guild);
-        await _modService.SilenceUsers(recentJoins, "Suspected raider");
 
         _generalStorage.ManualRaidSilence = true;
         await FileHelper.SaveGeneralStorageAsync(_generalStorage);
