4f500ff51a0f979dde582ac0beb2a8065128b103
ixrec <ericrhitchcock@gmail.com>
2023-08-08 00:02:54 +0100

    implement witties

--- Izzy-Moonbot/Settings/State.cs
+++ Izzy-Moonbot/Settings/State.cs
@@ -10,6 +10,8 @@ public class State
     public int CurrentLargeJoinCount = 0;
     public int CurrentSmallJoinCount = 0;
 
+    public DateTimeOffset LastWittyResponse = DateTimeOffset.MinValue;
+
     // AdminModule
     public DateTimeOffset LastMentionResponse = DateTimeOffset.MinValue;
 
