03b7ab41b51288aa5f09199f64421a13eb8ffb1d
ixrec <ericrhitchcock@gmail.com>
2023-11-08 23:29:23 +0000

    Revert "unbreak tests"
    This reverts commit 06b40b5c6883a2638772f368a91acf4b97d4fa7c.

--- Izzy-MoonbotTests/Tests/ModMiscModuleTests.cs
+++ Izzy-MoonbotTests/Tests/ModMiscModuleTests.cs
@@ -21,8 +21,7 @@ public class ModMiscModuleTests
         var ss = new ScheduleService(cfg, mod, modLog, logger, scheduledJobs);
 
         var users = new Dictionary<ulong, User>();
-        var state = new TransientState();
-        return (ss, new ModMiscModule(cfg, users, ss, logger, state));
+        return (ss, new ModMiscModule(cfg, users, ss, logger));
     }
 
     [TestMethod()]
