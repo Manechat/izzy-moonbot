fa6ba960571ecb934df8e89ba941c535d0ba3712
Neal Metcalf <3663541+LunarNightShade@users.noreply.github.com>
2023-11-08 20:44:16 -0600

    Revert "hotfix for user catchup spam after users.conf corruption"
    This reverts commit 21f7965a73bb3bef231936117a1026faff5a405a.

--- Izzy-Moonbot/EventListeners/UserListener.cs
+++ Izzy-Moonbot/EventListeners/UserListener.cs
@@ -167,13 +167,10 @@ public class UserListener
         
         var msg = $"{(catchingUp ? "Catching up on " : "")}Join: <@{member.Id}> (`{member.Id}`), created <t:{member.CreatedAt.ToUnixTimeSeconds()}:R>{autoSilence}{joinedBefore}{rolesAutoappliedString}";
         _logger.Log($"Generated moderation log for user join: ${msg}");
-        if (!catchingUp)
-        {
-            await _modLogger.CreateModLog(member.Guild)
-                .SetContent(msg)
-                .SetFileLogContent(msg)
-                .Send();
-        }
+        await _modLogger.CreateModLog(member.Guild)
+            .SetContent(msg)
+            .SetFileLogContent(msg)
+            .Send();
     }
     
     private async Task MemberLeaveEvent(SocketGuild guild, SocketUser user)
